ae3c5a657d1ad646ecf3aee26fb159b8b36bd078
nwachukwu ikedinachi <bldrnwagod@gmail.com>
2023-11-24 01:11:43 -0800

    increment,decrement,update and calculation logic

--- main.js
+++ main.js
@@ -4,56 +4,56 @@ let cartElement = document.getElementById("product__container");
 // shop items Datas
 let shopData = [
     {
-        id: 1,
+        id: "A",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 2,
+        id: "B",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 3,
+        id: "C",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 4,
+        id: "D",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 5,
+        id: "E",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 6,
+        id: "F",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 7,
+        id: "G",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
         price: 55
     },
     {
-        id: 8,
+        id: "H",
         img: "https://media.istockphoto.com/id/1453345677/photo/closeup-of-businessperson-in-wristwatch-businessperson-wearing-wristwatch-photo-of.webp?b=1&s=170667a&w=0&k=20&c=aqITAIOuzKedPY9LZo6LHN0ut2nfcjHX8vYJoavBXwQ=",
         title: "Mens Shirt",
         desc: "helloooooo",
@@ -64,9 +64,10 @@ let shopData = [
 let basket = [];
 
 // Generating items dynamically using js map function
-cartElement.innerHTML = shopData.map((item,idx) => {
-    let { id, img, title, desc, price } = item
-    return (` <div class="product__cart" id ="{idx}">
+cartElement.innerHTML = shopData.map((x) => {
+    let { id, img, title, desc, price } = x
+
+    return (` <div class="product__cart" id =product-id-${id}>
 
                     <div class="product__img">
                         <img src=${img} alt="">
@@ -80,9 +81,9 @@ cartElement.innerHTML = shopData.map((item,idx) => {
                         <h5>$ ${price}</h5>
 
                         <div class="product__buttons">
-                            <i class="bi bi-dash" onclick="decrement()"></i>
-                            <div class="product__quantity">0</div>
-                            <i class="bi bi-plus" onclick="increment()"></i>
+                            <i class="bi bi-dash" onclick="decrement(${id})"></i>
+                            <div id = ${id} class="product__quantity">0</div>
+                            <i class="bi bi-plus" onclick="increment(${id})"></i>
                         </div>
                         
                     </div>
@@ -94,20 +95,62 @@ cartElement.innerHTML = shopData.map((item,idx) => {
 
 
 // increment function
-const increment = (idx) => {
-    console.log(idx)
-   console.log("increment function is active") 
+const increment = (id) => {
+  
+    let selecteditem = id;
+
+    let search = basket.find((x) => 
+        x.id === selecteditem.id
+        );
+
+    if(search === undefined) {
+        basket.push({
+            id: selecteditem.id,
+            qty: 1
+        });
+    } else {
+        search.qty++;
+        
+    }
+
+
+    update(selecteditem.id);
+
 }
 
 // decrement function
-const decrement = () => {
-    console.log("decreement function is running")
+const decrement = (id) => {
+
+   let selecteditem = id;
+
+    let search = basket.find((x) => 
+        x.id === selecteditem.id
+        );
+
+    if (search.qty === 0) return; 
+     else {
+        search.qty--; 
+    }
+
+    update(selecteditem.id)
 }
 
 // update function
-const update = () => {
-   console.log("update functionality just started") 
+const update = (id) => {
+
+    let search = basket.find((x) => x.id === id);
+
+    console.log(search.qty);
+
+    document.getElementById(id).innerHTML = search.qty;
+
+    calculation()
+
 }
 
 
-console.log(cartElement);
+let calculation = () => {
+    let cartIcon = document.getElementById("cart__amount");
+
+    cartIcon.innerHTML = basket.map((x) => x.qty).reduce((x, y) => x + y);     
+}
